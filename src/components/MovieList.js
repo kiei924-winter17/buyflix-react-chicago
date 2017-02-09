1487a41e87135566e854bc2967529c02e5fe53c3
Brian Eng <brian@brianeng.com>
2017-02-08 19:59:42 -0600

    update

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,18 +2,19 @@ var React = require('react')
 var Movie = require('./Movie')
 
 var MovieList = React.createClass({
+  renderMovie: function(movie) {
+    return (
+      <Movie poster={movie.poster}
+             title={movie.title}
+             genre={movie.genre}
+             runtime={movie.runtime} />
+    )
+  },
   render: function() {
     return (
       <div className="movies col-sm-8">
         <div className="row">
-          {this.props.movies.map(function(movie) {
-            return (
-              <Movie poster={movie.poster}
-                     title={movie.title}
-                     genre={movie.genre}
-                     runtime={movie.runtime} />
-            )
-          })}
+          {this.props.movies.map(this.renderMovie)}
         </div>
       </div>
     )
